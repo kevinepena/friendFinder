2b574a0fa6cc096d8e593e3e2f9ace329fdb48a4
Kevin Pena <kevinpena160@gmail.com>
2018-05-24 22:12:14 -0500

    working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,13 +11,39 @@ module.exports = function (app) {
         // This works because of our body-parser middleware
         var newfriend = req.body;
 
-        console.log(newfriend);
+        var newAnswers = newfriend.scores;
+
+        var newName = "";
+        var newPic = "";
+
+        var totalDifference = 40;
+
+        for (var i = 0; i < friendsData.length; i++) {
+            var ran = 0;
+          
+
+            for (var j = 0; j < newAnswers.length; j++) {
+                
+                ran += Math.abs(friendsData[i].scores[j] - newAnswers[j]);
+
+                if (ran < totalDifference) {
+                    totalDifference = ran;
+                    newName = friendsData[i].name;
+                    newPic = friendsData[i].photo;
+                }
+            }
+
+
+        }
 
         friendsData.push(newfriend);
 
-        res.json(newfriend);
+        res.json({
+            matchName: newName, 
+            matchImage: newPic});
+ 
     });
 
-    
+
 
 }
